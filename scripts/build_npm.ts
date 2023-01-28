ddfa24e83bacda94cf9e7f30b5702dfb1b049198
lino-levan <11367844+lino-levan@users.noreply.github.com>
2023-01-28 13:45:43 -0800

    chore: move node readme to own file

--- scripts/build_npm.ts
+++ scripts/build_npm.ts
@@ -30,4 +30,4 @@ await build({
 
 // post build steps
 Deno.copyFileSync("LICENSE", "npm/LICENSE");
-Deno.copyFileSync("README.md", "npm/README.md");
+Deno.copyFileSync("scripts/README.md", "npm/README.md");
